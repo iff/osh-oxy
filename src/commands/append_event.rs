3c3b801511fbf3da4eca00304e3f3c3e8d498654
Yves Ineichen <iff@yvesineichen.com>
2024-03-22 18:21:03 +0100

    append_event: cleanup

--- src/commands/append_event.rs
+++ src/commands/append_event.rs
@@ -12,6 +12,22 @@ pub(crate) fn invoke(
     machine: &str,
     session: &str,
 ) -> anyhow::Result<()> {
+    // TODO maybe use hostname later, for now use our own file
+    let mut osh_file = home::home_dir().expect("home dir has to exist");
+    osh_file.push(".osh/active/local_oxy.osh");
+
+    if !osh_file.as_path().exists() {
+        // TODO default header?
+        append_json_lines(
+            osh_file.as_path(),
+            [Format {
+                format: String::from("osh-history-v1"),
+                description: None,
+            }],
+        )
+        .context("failed to serialise header")?;
+    }
+
     let e = Event {
         timestamp: Utc.timestamp_nanos((starttime * 1e9) as i64).into(),
         command: command.to_string(),
@@ -21,22 +37,7 @@ pub(crate) fn invoke(
         machine: machine.to_string(),
         session: session.to_string(),
     };
-
-    // TODO maybe use hostname later, for now use our own file
-    let mut home = home::home_dir().expect("home dir has to exist");
-    home.push(".osh");
-    home.push("active");
-    home.push("local_oxy.osh");
-
-    if !home.as_path().exists() {
-        // TODO switch to v2 and use tagged?
-        let format = Format {
-            format: String::from("osh-history-v1"),
-            description: None,
-        };
-        append_json_lines(home.as_path(), [format]).context("failed to serialise header")?;
-    }
-    append_json_lines(home.as_path(), [Entry::EventE { event: e }])
+    append_json_lines(osh_file.as_path(), [Entry::EventE { event: e }])
         .context("failed to serialise event")?;
 
     Ok(())
